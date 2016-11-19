5c5a9235fa2d1b561545999a60c24a5762dbbe8a
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-11-19 13:01:43 -0800

    Adds Store Unit Tests

---
+++ src/test/java/tech/blackhole/blacknectar/service/stores/StoreTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2016 BlackWholeLabs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.blackhole.blacknectar.service.stores;
+
+import com.google.gson.JsonObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+import tech.sirwellington.alchemy.test.junit.runners.GeneratePojo;
+import tech.sirwellington.alchemy.test.junit.runners.Repeat;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Repeat(10)
+@RunWith(AlchemyTestRunner.class)
+public class StoreTest 
+{
+
+    @GeneratePojo
+    private Store instance;
+    
+    @Before
+    public void setUp() throws Exception
+    {
+        setupData();
+    }
+
+
+    private void setupData() throws Exception
+    {
+        
+    }
+
+    @Test
+    public void testAsJSON()
+    {
+        JsonObject json = instance.asJSON();
+        assertThat(json, notNullValue());
+        
+    }
+}

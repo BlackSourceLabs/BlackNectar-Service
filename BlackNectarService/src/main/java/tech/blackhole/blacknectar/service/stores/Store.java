070ecb45161440310e8db6ab0281b8c54b149ee0
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-11-18 23:48:36 -0800

    Adds a basic Store POJO

---
+++ BlackNectarService/src/main/java/tech/blackhole/blacknectar/service/stores/Store.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016 BlackWholeLabs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ 
+package tech.blackhole.blacknectar.service.stores;
+
+
+import tech.sirwellington.alchemy.annotations.concurrency.Immutable;
+import tech.sirwellington.alchemy.annotations.concurrency.ThreadSafe;
+import tech.sirwellington.alchemy.annotations.objects.Pojo;
+
+/**
+ *
+ * @author SirWellington
+ */
+@Pojo
+@Immutable
+@ThreadSafe
+public class Store 
+{
+    private final String name;
+    private final Location location;
+    private final Address address;
+
+    public Store(String name, Location location, Address address)
+    {
+        this.name = name;
+        this.location = location;
+        this.address = address;
+    }
+    
+    
+}

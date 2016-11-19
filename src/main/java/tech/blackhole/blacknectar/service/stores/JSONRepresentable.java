b87f129c334f52dd70275ee712c2682a4f539351
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-11-19 09:50:25 -0800

    Adds JSONRepresentable interface

---
+++ src/main/java/tech/blackhole/blacknectar/service/stores/JSONRepresentable.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2016 BlackWholeLabs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package tech.blackhole.blacknectar.service.stores;
+
+import com.google.gson.JsonObject;
+
+
+/**
+ * Represents a POJO that can be represented as JSON (using the GSON library).
+ * 
+ * @author SirWellington
+ */
+public interface JSONRepresentable 
+{
+    JsonObject asJSON();
+}

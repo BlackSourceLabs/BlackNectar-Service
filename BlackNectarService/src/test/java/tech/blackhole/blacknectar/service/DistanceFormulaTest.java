e136d6f64f9c69da2da63ba3d891a69231f3bdb8
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-11-19 00:03:02 -0800

    Updates DistanceFormulaUnitTest

--- BlackNectarService/src/test/java/tech/blackhole/blacknectar/service/DistanceFormulaTest.java
+++ BlackNectarService/src/test/java/tech/blackhole/blacknectar/service/DistanceFormulaTest.java
@@ -20,49 +20,63 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import tech.blackhole.blacknectar.service.stores.Location;
+import tech.sirwellington.alchemy.generator.AlchemyGenerator;
 import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
 import tech.sirwellington.alchemy.test.junit.runners.DontRepeat;
 import tech.sirwellington.alchemy.test.junit.runners.Repeat;
 
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
+import static tech.sirwellington.alchemy.generator.NumberGenerators.doubles;
 import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
 
 /**
  *
  * @author SirWellington
  */
-@Repeat(10)
+@Repeat(100)
 @RunWith(AlchemyTestRunner.class)
 public class DistanceFormulaTest
 {
 
     private DistanceFormula formula;
-
+    
+    private Location first;
+    private Location second;
+    
     @Before
     public void setUp() throws Exception
     {
+        formula = new DistanceFormula.HarvesineDistance();
 
         setupData();
-        setupMocks();
     }
 
     private void setupData() throws Exception
     {
-
+        AlchemyGenerator<Double> latitudes = doubles(-90, 90.0);
+        AlchemyGenerator<Double> longitudes = doubles(-180, 180);
+        
+        first = new Location(latitudes.get(), longitudes.get());
+        second = new Location(latitudes.get(), longitudes.get());
     }
 
-    private void setupMocks() throws Exception
+    @Test
+    public void testHarvesine()
     {
-
+        double distance = formula.distanceBetween(first, second);
+        assertThat(distance, greaterThan(0.0));
     }
 
     @DontRepeat
     @Test
-    public void testHarvesine()
+    public void testHarvesineAccuracy()
     {
         formula = new DistanceFormula.HarvesineDistance();
 
-        Location first = new Location(36.12, -86.67);
-        Location second = new Location(33.94, -118.40);
+        first = new Location(36.12, -86.67);
+        second = new Location(33.94, -118.40);
 
         double expected = 2887.2599506071124;
         double result = formula.distanceBetween(first, second);

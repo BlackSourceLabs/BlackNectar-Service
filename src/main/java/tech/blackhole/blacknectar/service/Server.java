d5d8c85d746d27cf48e6000f13618e7ac3c51b19
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-11-19 21:15:23 -0800

    Adds support for query parameters when searching for stores

--- src/main/java/tech/blackhole/blacknectar/service/Server.java
+++ src/main/java/tech/blackhole/blacknectar/service/Server.java
@@ -18,16 +18,22 @@ package tech.blackhole.blacknectar.service;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.QueryParamsMap;
 import spark.Request;
 import spark.Response;
 import spark.Spark;
 import tech.aroma.client.Aroma;
 import tech.aroma.client.Urgency;
+import tech.blackhole.blacknectar.service.stores.Location;
 import tech.blackhole.blacknectar.service.stores.Store;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static com.google.common.base.Strings.nullToEmpty;
 
 /**
  *
@@ -131,9 +137,144 @@ public final class Server
         BiConsumer<JsonArray, JsonObject> accumulator = (array, object) -> array.add(object);
         BiConsumer<JsonArray, JsonArray> combiner = (first, second) -> first.addAll(second);
 
-        return service.getAllStores()
-            .stream()
+        List<Store> stores = findStores(request);
+        
+        return stores.stream()
             .map(Store::asJSON)
             .collect(supplier, accumulator, combiner);
+    }
+
+    private List<Store> findStores(Request request)
+    {
+        QueryParamsMap queryParams = request.queryMap();
+        
+        Location center = null;
+        
+        if (hasLocationParameters(queryParams))
+        {
+            double latitude = queryParams.get(QueryKeys.LATITUDE).doubleValue();
+            double longitude = queryParams.get(QueryKeys.LONGITUDE).doubleValue();
+            center = new Location(latitude, longitude);
+        }
+        
+        int limit = 0;
+        if (hasLimitParameter(queryParams))
+        {
+            limit = queryParams.get(QueryKeys.LIMIT).integerValue();
+            if (limit <0 )
+            {
+                LOG.warn("Unexpected limit received: {}", limit);
+                
+                AROMA.begin()
+                    .titled("Bad Argument")
+                    .text("Unexpected limit received: {}", limit)
+                    .send();
+                
+                limit = 0;
+            }
+        }
+        
+        String searchTerm = "";
+        if (hasSearchTermParameter(queryParams))
+        {
+            searchTerm = queryParams.value(QueryKeys.SEARCH_TERM);
+            searchTerm = nullToEmpty(searchTerm);
+        }
+        
+        double radius = -1.0;
+        if (hasRadiusParameter(queryParams))
+        {
+            radius = queryParams.get(QueryKeys.RADIUS).doubleValue();
+        }
+        
+        //Search Term, Center, Radius, Limit
+        if (hasSearchTerm(searchTerm) && 
+            hasLocationParameters(queryParams) &&
+            hasLimit(limit) &&
+            hasRadius(radius))
+        {
+            return service.searchForStoresByName(searchTerm, center, radius, limit);
+        }
+        
+        //Search Term, Center, Radius
+        if (hasSearchTerm(searchTerm) &&
+            hasLocationParameters(queryParams) &&
+            hasRadius(radius))
+        {
+            return service.searchForStoresByName(searchTerm, center, radius);
+        }
+        
+        //Center, Radius, Limit
+        if (hasLocationParameters(queryParams) && 
+            hasRadius(radius) &&
+            hasLimit(limit))
+        {
+            return service.searchForStoresByLocation(center, radius, limit);
+        }
+        
+        //Center, Radius
+        if (hasLocationParameters(queryParams) && hasRadius(radius))
+        {
+            return service.searchForStoresByLocation(center, radius);
+        }
+        
+        //Search Term
+        if (hasSearchTerm(searchTerm))
+        {
+            return service.searchForStoresByName(searchTerm);
+        }
+        
+        //Limit
+        if (hasLimit(limit))
+        {
+            return service.getAllStores(limit);
+        }
+        
+        return service.getAllStores();
+    }
+
+    private boolean hasLocationParameters(QueryParamsMap queryParams)
+    {
+        return queryParams.hasKey(QueryKeys.LATITUDE) &&
+               queryParams.hasKey(QueryKeys.LONGITUDE);
+    }
+
+    private boolean hasLimitParameter(QueryParamsMap queryParams)
+    {
+        return queryParams.hasKey(QueryKeys.LIMIT);
+    }
+
+    private boolean hasSearchTermParameter(QueryParamsMap queryParams)
+    {
+        return queryParams.hasKey(QueryKeys.SEARCH_TERM);
+    }
+
+    private boolean hasRadiusParameter(QueryParamsMap queryParams)
+    {
+        return queryParams.hasKey(QueryKeys.RADIUS);
+    }
+
+    private boolean hasLimit(int limit)
+    {
+        return limit > 0;
+    }
+
+    private boolean hasSearchTerm(String searchTerm)
+    {
+        return !isNullOrEmpty(searchTerm);
+    }
+
+    private boolean hasRadius(double radius)
+    {
+        return radius > 0.0;
+    }
+    
+    static class QueryKeys
+    {
+        static final String LATITUDE = "lat";
+        static final String LONGITUDE = "lon";
+        static final String LIMIT = "limit";
+        static final String RADIUS = "radius";
+        static final String SEARCH_TERM = "searchTerm";
     }
 }

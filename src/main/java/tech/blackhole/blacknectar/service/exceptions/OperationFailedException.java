6658b3ca583d7f4532ee0c2935ecfe6413859235
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-11-20 10:21:42 -0800

    Moves Exception into exceptions pacakge

--- src/main/java/tech/blackhole/blacknectar/service/exceptions/OperationFailedException.java
+++ src/main/java/tech/blackhole/blacknectar/service/exceptions/OperationFailedException.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package tech.blackhole.blacknectar.service;
+package tech.blackhole.blacknectar.service.exceptions;
 
 /**
  * Thrown when an API call fails.

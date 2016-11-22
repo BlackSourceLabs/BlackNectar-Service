1da9c3389b3234dbfe5901a1ca5d0ddd9e1347d6
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-11-21 21:26:27 -0800

    Adds ebt_store schema

---
+++ src/main/sql/create_ebt_stores.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS stores
+(
+    store_name text,
+    latitude numeric,
+    longitude numeric,
+    address text,
+    address_line_two text,
+    city text,
+    state text,
+    county text,
+    zip text,
+    local_zip text,
+
+    PRIMARY KEY(store_name, latitude, longitude)
+);

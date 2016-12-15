414dbd71dcfd1d49422b7fcb618f2e1d6ab87084
Wellington Moreno <jwellington.moreno@gmail.com>
2016-12-14 16:17:17 -0800

    Creates ModuleImageLoader

---
+++ src/main/java/tech/blacksource/blacknectar/service/api/images/ModuleImageLoader.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2016 BlackSourceLabs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ 
+package tech.blacksource.blacknectar.service.api.images;
+
+
+import com.google.inject.AbstractModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author SirWellington
+ */
+public final class ModuleImageLoader extends AbstractModule
+{
+    private final static Logger LOG = LoggerFactory.getLogger(ModuleImageLoader.class);
+
+    @Override
+    protected void configure()
+    {
+        bind(ImageLoader.class).annotatedWith(Yelp.class).to(YelpImageLoader.class);
+    }
+    
+}

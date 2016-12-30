fca2337224027e41389b64fe302d2d3a3a45210c
Wellington Moreno <jwellington.moreno@gmail.com>
2016-12-30 13:59:35 -0800

    #17 - Fixes StoreRepository.FILE

--- src/main/java/tech/blacksource/blacknectar/service/stores/StoreRepository.java
+++ src/main/java/tech/blacksource/blacknectar/service/stores/StoreRepository.java
@@ -35,5 +35,5 @@ public interface StoreRepository
      */
     List<Store> getAllStores();
     
-    static StoreRepository FILE = new FileRepository(Aroma.create());
+    static StoreRepository FILE = new FileRepository(Aroma.create(), IDGenerator.uuids());
 }

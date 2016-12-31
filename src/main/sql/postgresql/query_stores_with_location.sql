9512498cc4f4c9587141acca69e1d2c922ba72b8
Wellington Moreno <jwellington.moreno@gmail.com>
2016-12-31 08:32:05 -0800

    Adds SQL Query

---
+++ src/main/sql/postgresql/query_stores_with_location.sql
@@ -0,0 +1,9 @@
+-- Find Stores around a Geolocation
+-- Returns resulting stores and the distance from the specified GeoLocation
+
+SELECT
+	*,
+	ST_Distance(location, ST_SetSRID(ST_Point(?, ?), 4326)) AS distance_meters
+FROM BlackNectar.Stores
+WHERE ST_DWithin(location, ST_SetSRID(ST_Point(?, ?), 4326), ?)
+ORDER BY distance

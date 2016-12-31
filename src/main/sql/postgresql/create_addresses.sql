8713b7bda5d0b41a22744b6a30de08f1d7ea826a
Wellington Moreno <jwellington.moreno@gmail.com>
2016-12-31 08:49:38 -0800

    #17 - Updates Table Schemas to use Text instead of VARCHAR

--- src/main/sql/postgresql/create_addresses.sql
+++ src/main/sql/postgresql/create_addresses.sql
@@ -5,11 +5,11 @@
 CREATE TABLE IF NOT EXISTS Store_Addresses
 (
 	address_id uuid PRIMARY KEY,
-	address_line_one VARCHAR(100) NOT NULL,
-	address_line_two VARCHAR(100),
-	city VARCHAR(100),
-	state VARCHAR(5),
-	county VARCHAR(100),
+	address_line_one TEXT NOT NULL,
+	address_line_two TEXT,
+	city TEXT,
+	state TEXT,
+	county TEXT,
 	zip_code INTEGER,
 	local_zip_code INTEGER,
 
--- src/main/sql/postgresql/create_stores.sql
+++ src/main/sql/postgresql/create_stores.sql
@@ -4,17 +4,17 @@
 CREATE TABLE IF NOT EXISTS BlackNectar.Stores
 (
 		store_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    store_name VARCHAR(100),
+    store_name TEXT,
     latitude NUMERIC NOT NULL,
     longitude NUMERIC NOT NULL,
     location GEOGRAPHY,
-    address_line_one VARCHAR(100),
-    address_line_two VARCHAR(100),
-    city VARCHAR(100),
-    state VARCHAR(5),
-    county VARCHAR(100),
-    zip_code VARCHAR(20),
-    local_zip_code VARCHAR(20),
+    address_line_one TEXT
+    address_line_two TEXT,
+    city TEXT,
+    state TEXT,
+    county TEXT,
+    zip_code TEXT,
+    local_zip_code TEXT,
 
     CONSTRAINT Unique_Stores UNIQUE(store_name, latitude, longitude)
 );

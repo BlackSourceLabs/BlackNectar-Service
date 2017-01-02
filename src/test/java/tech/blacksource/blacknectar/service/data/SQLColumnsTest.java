6c8600af8c94637f9dad2104a7fb8e15a2cfcab9
Wellington Moreno <jwellington.moreno@gmail.com>
2017-01-02 00:21:08 -0800

    Adds new SQL Columns

---
+++ src/test/java/tech/blacksource/blacknectar/service/data/SQLColumnsTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2017 BlackSourceLabs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.blacksource.blacknectar.service.data;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+
+import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
+
+/**
+ *
+ * @author SirWellington
+ */
+@RunWith(AlchemyTestRunner.class)
+public class SQLColumnsTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+        setupData();
+        setupMocks();
+    }
+
+    private void setupData() throws Exception
+    {
+
+    }
+
+    private void setupMocks() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testCannotInstatiate() throws Exception
+    {
+        assertThrows(() -> new SQLColumns());
+    }
+
+}

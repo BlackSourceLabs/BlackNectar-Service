557c54bb51c1b69e668967d97ccd6232719ace96
Wellington Moreno <jwellington.moreno@gmail.com>
2017-01-03 01:05:35 -0800

    #18 - Updates Store Images schema and queries

--- src/main/resources/sql/postgresql/create_store_images.sql
+++ src/main/resources/sql/postgresql/create_store_images.sql
@@ -5,8 +5,16 @@ CREATE TABLE IF NOT EXISTS Store_Images
 (
 		store_id uuid REFERENCES Stores(store_id),
 		image_id text,
-		-- Which type of image is it? Cover Image? Alternative Image? Food Image? Etc.
-		image_type text,
+		-- This is otherwise known as the image blob
+		image_binary BYTEA,
+		height INT,
+		width INT,
+		size_in_bytes INT,
+		content_type TEXT,
+		-- JPG, PNG, etc
+		image_type TEXT,
+		source TEXT,
+		url TEXT,
 
 		PRIMARY KEY (store_id, image_id)
 );
--- src/main/resources/sql/postgresql/insert_store_image.sql
+++ src/main/resources/sql/postgresql/insert_store_image.sql
@@ -1,3 +1,3 @@
-INSERT INTO store_images
-(store_id, image_id, image_type)
-VALUES (?, ?, ?)
+INSERT INTO Store_Images
+(store_id, image_id, image_binary, height, width, size_in_bytes, content_type, image_type, source, url)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)

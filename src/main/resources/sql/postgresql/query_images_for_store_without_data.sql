28a9a57201fd7fe267db4e4d43177794dc9deb78
Wellington Moreno <jwellington.moreno@gmail.com>
2017-01-04 23:20:43 -0800

    #18 - Adds new SQL Queries

---
+++ src/main/resources/sql/postgresql/query_images_for_store_without_data.sql
@@ -0,0 +1,8 @@
+-- Find Images for a particular store
+-- Returns resulting images of the specified store, but without the image_binary data.
+-- This is done to save space in the case where only the image metadata is wanted.
+-- ===========================================================================
+
+SELECT store_id, image_id, height, width, size_in_bytes, content_type, image_type, source, url
+FROM blacknectar.store_images
+WHERE store_id = ?;

9a93f64278b2dd13d85d5f80afc70a495f5ded14
Wellington Moreno <jwellington.moreno@gmail.com>
2017-01-10 01:53:23 -0800

    #37 - Adds Unit Tests for GooglePlacesMatchingAlgorithm

--- src/main/java/tech/blacksource/blacknectar/service/algorithms/GooglePlacesMatchingAlgorithm.java
+++ src/main/java/tech/blacksource/blacknectar/service/algorithms/GooglePlacesMatchingAlgorithm.java
@@ -16,6 +16,8 @@
 
 package tech.blacksource.blacknectar.service.algorithms;
 
+import java.util.Objects;
+import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.aroma.client.Aroma;
@@ -38,88 +40,98 @@ import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull
 @StrategyPattern(role = CONCRETE_BEHAVIOR)
 final class GooglePlacesMatchingAlgorithm implements StoreMatchingAlgorithm<Place>
 {
-    
+
     private final static Logger LOG = LoggerFactory.getLogger(GooglePlacesMatchingAlgorithm.class);
-    
+
     private final Aroma aroma;
-    
+
+    @Inject
     GooglePlacesMatchingAlgorithm(Aroma aroma)
     {
         checkThat(aroma).is(notNull());
-        
+
         this.aroma = aroma;
     }
-    
+
     @Override
     public boolean matchesStore(Place candidate, Store store)
     {
+        
+        checkThat(candidate, store)
+            .usingMessage("arguments cannot be null")
+            .are(notNull());
+        
         if (namesMatch(candidate.name, store.getName()))
         {
             makeNoteThatNamesMatch(candidate, store);
             return true;
         }
-        
+
         if (addressesMatch(candidate, store))
         {
             makeNoteThatAddressesMatch(candidate, store);
             return true;
         }
-        
+
         return false;
-        
+
     }
-    
+
     private boolean namesMatch(String first, String second)
     {
         if (Checks.anyAreNullOrEmpty(first, second))
         {
             return false;
         }
-        
+
         first = first.toLowerCase();
         second = second.toLowerCase();
+
+        if (Objects.equals(first, second))
+        {
+            return true;
+        }
         
         return first.contains(second) || second.contains(first);
     }
-    
+
     private boolean addressesMatch(Place place, Store store)
     {
         if (!place.hasFormattedAddress() && !place.hasVicinity())
         {
             return false;
         }
-        
+
         Address address = store.getAddress();
-        
+
         if (namesMatch(place.vicinity, address.getAddressLineOne()))
         {
             return true;
         }
-        
+
         return namesMatch(place.formattedAddress, address.getAddressLineOne()) &&
                namesMatch(place.formattedAddress, address.getCity()) &&
-               namesMatch(place.formattedAddress, address.getState()) &&
-               namesMatch(place.formattedAddress, address.getZipCode());
+               namesMatch(place.formattedAddress, address.getState());
     }
-    
+
     private void makeNoteThatNamesMatch(Place place, Store store)
     {
         LOG.debug("Place matches store by name: {}", place);
-        
+
         aroma.begin().titled("Matched By Name")
             .text("Google Place matches store by name.\n\nStore:\n{}\n\nPlace:\n{}", store, place)
             .withUrgency(Urgency.LOW)
             .send();
     }
-    
+
     private void makeNoteThatAddressesMatch(Place place, Store store)
     {
         LOG.debug("Place matches store by address: {}", place);
-        
+
         aroma.begin().titled("Matched By Address")
             .text("Google Place matches store by address.\n\nStore:\n{}\n\nPlace:\n{}", store, place)
             .withUrgency(Urgency.LOW)
             .send();
     }
-    
+
 }
--- src/test/java/tech/blacksource/blacknectar/service/algorithms/GooglePlacesMatchingAlgorithmTest.java
+++ src/test/java/tech/blacksource/blacknectar/service/algorithms/GooglePlacesMatchingAlgorithmTest.java
@@ -19,12 +19,23 @@ package tech.blacksource.blacknectar.service.algorithms;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import tech.aroma.client.Aroma;
+import tech.blacksource.blacknectar.service.stores.Address;
 import tech.blacksource.blacknectar.service.stores.Store;
 import tech.redroma.google.places.data.Place;
 import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
+import tech.sirwellington.alchemy.test.junit.runners.GeneratePojo;
 import tech.sirwellington.alchemy.test.junit.runners.GenerateString;
 import tech.sirwellington.alchemy.test.junit.runners.Repeat;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Answers.RETURNS_MOCKS;
+import static tech.blacksource.blacknectar.service.BlackNectarGenerators.stores;
+import static tech.sirwellington.alchemy.generator.AlchemyGenerator.one;
+import static tech.sirwellington.alchemy.generator.BooleanGenerators.booleans;
+import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.assertThrows;
 
 /**
  *
@@ -32,37 +43,109 @@ import tech.sirwellington.alchemy.test.junit.runners.Repeat;
  */
 @Repeat(10)
 @RunWith(AlchemyTestRunner.class)
-public class GooglePlacesMatchingAlgorithmTest 
+public class GooglePlacesMatchingAlgorithmTest
 {
+
+    @Mock(answer = RETURNS_MOCKS)
+    private Aroma aroma;
 
     @GenerateString
     private String storeName;
-    
+
     private Store store;
+
+    @GeneratePojo
+    private Place randomPlace;
+
+    @GeneratePojo
     private Place place;
-    
+
+    private GooglePlacesMatchingAlgorithm instance;
+
     @Before
     public void setUp() throws Exception
     {
-        
+
         setupData();
         setupMocks();
-    }
 
+        instance = new GooglePlacesMatchingAlgorithm(aroma);
+    }
 
     private void setupData() throws Exception
     {
-        
+        store = one(stores());
     }
 
     private void setupMocks() throws Exception
     {
-        
+
     }
 
     @Test
-    public void testMatchesStore()
+    public void testMatchesStoreWhenNameMatch() throws Exception
     {
+        updatePlaceToMatchStoreName(place, store);
+
+        boolean result = instance.matchesStore(place, store);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testMatchesStoreWhenAddressMatch() throws Exception
+    {
+        updatePlaceToMatchStoreAddress(place, store);
+
+        boolean result = instance.matchesStore(place, store);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testMatchesStoreWhenNoMatch()
+    {
+        boolean result = instance.matchesStore(randomPlace, store);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testmatchesStoreWithBadArgs() throws Exception
+    {
+        assertThrows(() -> instance.matchesStore(null, store)).isInstanceOf(IllegalArgumentException.class);
+        assertThrows(() -> instance.matchesStore(place, null)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private void updatePlaceToMatchStoreName(Place place, Store store)
+    {
+        boolean shouldEqual = one(booleans());
+
+        if (shouldEqual)
+        {
+            place.name = store.getName();
+        }
+        else
+        {
+            place.name += store.getName();
+        }
+    }
+
+    private void updatePlaceToMatchStoreAddress(Place place, Store store)
+    {
+        Address storeAddress = store.getAddress();
+        boolean useVicinity = one(booleans());
+
+        if (useVicinity)
+        {
+            place.vicinity = storeAddress.getAddressLineOne();
+        }
+
+        else
+        {
+            place.formattedAddress = storeAddress.getAddressLineOne() + " " +
+                                     storeAddress.getAddressLineTwo() + " " + 
+                                     storeAddress.getCity() + " " + 
+                                     storeAddress.getState();
+        }
+
     }
 
 }

5c47fee7415ad8164d6ab1e6eca25b2713023868
Wellington Moreno <jwellington.moreno@gmail.com>
2017-01-10 13:21:17 -0800

    #38 - Updates SQL Query

--- src/main/resources/sql/postgresql/query_stores_with_name_and_location.sql
+++ src/main/resources/sql/postgresql/query_stores_with_name_and_location.sql
@@ -3,9 +3,11 @@
 -- ===========================================================================
 
 SELECT
-	*,
+	Stores.*,
+	Store_Images.url,
 	ST_Distance(location, ST_SetSRID(ST_Point(?, ?), 4326)::geography) AS distance_meters
 FROM Stores
+LEFT JOIN Store_Images USING(store_id)
 WHERE store_name LIKE ?
 AND ST_DWithin(location, ST_SetSRID(ST_Point(?, ?), 4326)::geography, ?)
 ORDER BY distance_meters

de3fbb07687cb75913b40bce46bd94597d8deae4
Wellington Moreno <jwellington.moreno@gmail.com>
2017-01-15 11:35:07 -0800

    #48 - Updates 'update_store' SQL

--- src/main/resources/sql/postgresql/update_store.sql
+++ src/main/resources/sql/postgresql/update_store.sql
@@ -1,5 +1,16 @@
 -- Updates an existing Store with new information
 -- ===========================================================================
 UPDATE Stores
-SET store_id=?, store_name=?, latitude=?, longitude=?, location=?, address_line_one=?, address_line_two=?, city=?, state=?, county=?, zip_code=?, local_zip_code=?, store_code=?
+SET store_id=?,
+    store_name=?,
+    latitude=?,
+    longitude=?,
+    location=public.ST_SetSRID(public.ST_Point(?, ?), 4326)::public.geography,
+    address_line_one=?,
+    address_line_two=?,
+    city=?,
+    state=?,
+    county=?,
+    zip_code=?,
+    local_zip_code=?
 WHERE store_id = ?

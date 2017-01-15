284481ceb327a7d283ce64ee436b71264d32e699
Wellington Moreno <jwellington.moreno@gmail.com>
2017-01-15 11:33:18 -0800

    #48 - Updates TestingResoures with TestDatabase

--- src/test/java/tech/blacksource/blacknectar/service/TestingResources.java
+++ src/test/java/tech/blacksource/blacknectar/service/TestingResources.java
@@ -35,7 +35,7 @@ public class TestingResources
 {
     private final static Logger LOG = LoggerFactory.getLogger(TestingResources.class);
 
-    private static final Injector INJECTOR = Guice.createInjector(new ModuleServer());
+    private static final Injector INJECTOR = Guice.createInjector(new ModuleServer(), new ModuleTestingDatabase());
 
     public static Injector createInjector()
     {

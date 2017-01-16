4f36fa1838db3b770782e3e195b1a91dfb165ba1
Wellington Moreno <jwellington.moreno@gmail.com>
2017-01-15 23:25:38 -0800

    #44 - Adds missing guice module to RunSearchYelpImages

--- src/main/java/tech/blacksource/blacknectar/service/RunSearchYelpImages.java
+++ src/main/java/tech/blacksource/blacknectar/service/RunSearchYelpImages.java
@@ -67,7 +67,7 @@ public final class RunSearchYelpImages implements Callable<Void>
     
     public static void main(String[] args) throws Exception
     {
-        Injector injector = Guice.createInjector(new ModuleServer());
+        Injector injector = Guice.createInjector(new ModuleServer(), new ModuleProductionDatabase());
         
         RunSearchYelpImages instance = injector.getInstance(RunSearchYelpImages.class);
         instance.call();

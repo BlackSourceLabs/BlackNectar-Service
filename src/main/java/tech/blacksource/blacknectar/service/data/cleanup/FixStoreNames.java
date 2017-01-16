4465f1245ccc769b88ca0ad0157df2181d7af90a
Wellington Moreno <jwellington.moreno@gmail.com>
2017-01-15 20:51:49 -0800

    #46 - Adds scrip that fixes store name issues

---
+++ src/main/java/tech/blacksource/blacknectar/service/data/cleanup/FixStoreNames.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2017 BlackSource, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tech.blacksource.blacknectar.service.data.cleanup;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.blacksource.blacknectar.service.ModuleProductionDatabase;
+import tech.blacksource.blacknectar.service.ModuleServer;
+
+/**
+ * Fixes issues with stores where some of them are in ALL CAPS.
+ * 
+ * @author SirWellington
+ */
+public final class FixStoreNames
+{
+    
+    private final static Logger LOG = LoggerFactory.getLogger(FixStoreNames.class);
+    
+    public static void main(String[] args) throws Exception
+    {
+        Injector injector = Guice.createInjector(new ModuleServer(), 
+                                                 new ModuleProductionDatabase(),
+                                                 new ModuleTitleCaseTransformation());
+        
+        RunStoreTransformation instance = injector.getInstance(RunStoreTransformation.class);
+        
+        instance.call();
+    }
+    
+    private static class ModuleTitleCaseTransformation extends AbstractModule
+    {
+        
+        @Override
+        protected void configure()
+        {
+            bind(StoreTransformation.class).to(TitleCaseStoreNamesTransformation.class);
+        }
+    }
+    
+}

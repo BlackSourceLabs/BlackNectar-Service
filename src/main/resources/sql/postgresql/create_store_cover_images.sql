1d5337bea17b272bddbdfcb32971a5c901cf70b6
Wellington Moreno <jwellington.moreno@gmail.com>
2017-02-06 17:42:04 -0800

    #61 - Adds new Cover Image Table

---
+++ src/main/resources/sql/postgresql/create_store_cover_images.sql
@@ -0,0 +1,12 @@
+-- Used to store the main Cover image associated with a Store
+-- ===========================================================================
+
+CREATE TABLE IF NOT EXISTS Store_Cover_Images
+(
+		store_id uuid REFERENCES Stores(store_id),
+		image_id text NOT NULL,
+
+		CONSTRAINT foreign_key FOREIGN KEY(store_id, image_id) REFERENCES Store_Images(store_id, image_id),
+
+		PRIMARY KEY (store_id)
+);

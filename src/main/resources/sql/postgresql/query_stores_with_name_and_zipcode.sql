771be47ae5479c0b8af2132533ed0cf8dcd203ad
Wellington Moreno <jwellington.moreno@gmail.com>
2017-02-08 22:36:58 -0800

    #34 - Adds new Query

---
+++ src/main/resources/sql/postgresql/query_stores_with_name_and_zipcode.sql
@@ -0,0 +1,13 @@
+-- Find Stores that match zip code
+-- Returns resulting stores that match the specified zip code.
+-- ===========================================================================
+
+SELECT
+	Stores.*,
+	Store_Images.url
+FROM Stores
+LEFT JOIN Store_Cover_Images USING(store_id)
+LEFT JOIN Store_Images USING(store_id, image_id)
+WHERE zip_code = ?
+AND store_name LIKE ?
+ORDER BY store_name ASC

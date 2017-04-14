79d33cb2baf714573840025e45c6388c7a462db7
Wellington Moreno <jwellington.moreno@gmail.com>
2017-04-14 16:53:05 -0700

    Reformats

--- src/main/java/tech/blacksource/blacknectar/service/algorithms/GooglePlacesStoreSearchAlgorithm.java
+++ src/main/java/tech/blacksource/blacknectar/service/algorithms/GooglePlacesStoreSearchAlgorithm.java
@@ -39,60 +39,59 @@ import static tech.sirwellington.alchemy.arguments.Arguments.*;
 import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull;
 
 /**
- *
  * @author SirWellington
  */
 @StrategyPattern(role = CONCRETE_BEHAVIOR)
 class GooglePlacesStoreSearchAlgorithm implements StoreSearchAlgorithm<Place>
 {
-    
+
     private final static Logger LOG = LoggerFactory.getLogger(GooglePlacesStoreSearchAlgorithm.class);
     static final int DEFAULT_RADIUS = 4_000;
-    
+
     private final Aroma aroma;
     private final GooglePlacesAPI google;
     private final StoreMatchingAlgorithm<Place> matchingAlgorithm;
-    
+
     @Inject
     GooglePlacesStoreSearchAlgorithm(Aroma aroma, GooglePlacesAPI google, StoreMatchingAlgorithm<Place> matchingAlgorithm)
     {
         checkThat(aroma, google, matchingAlgorithm)
-            .are(notNull());
-        
+                .are(notNull());
+
         this.aroma = aroma;
         this.google = google;
         this.matchingAlgorithm = matchingAlgorithm;
     }
-    
+
     @Override
     public Place findMatchFor(Store store)
     {
         checkStore(store);
-        
+
         List<Place> places = tryToGetPlacesFor(store);
         if (Lists.isEmpty(places))
         {
             return null;
         }
-        
+
         Place result = tryToFindMatchingPlaceFor(store, places);
-        
+
         if (result == null)
         {
             makeNoteThatNoMatchesFoundFor(store, places);
         }
-        else        
+        else
         {
             makeNoteThatPlaceMatched(store, result);
         }
-        
+
         return result;
     }
-    
+
     private List<Place> tryToGetPlacesFor(Store store)
     {
         NearbySearchRequest request = createRequestToSearchFor(store);
-        
+
         List<Place> places;
         try
         {
@@ -103,79 +102,79 @@ class GooglePlacesStoreSearchAlgorithm implements StoreSearchAlgorithm<Place>
             makeNoteThatGoogleSearchFailed(ex, request, store);
             throw new OperationFailedException("Could not make Google API Request", ex);
         }
-        
+
         if (Lists.isEmpty(places))
         {
             makeNoteThatNoPlacesFoundFor(store, request);
         }
-        
+
         return Lists.nullToEmpty(places);
     }
-    
+
     private Place tryToFindMatchingPlaceFor(Store store, List<Place> places)
     {
         return places.stream()
-            .filter(place -> matchingAlgorithm.matchesStore(place, store))
-            .findFirst()
-            .orElse(null);
+                     .filter(place -> matchingAlgorithm.matchesStore(place, store))
+                     .findFirst()
+                     .orElse(null);
     }
-    
+
     private NearbySearchRequest createRequestToSearchFor(Store store)
     {
         Location location = Location.of(store.getLocation().getLatitude(), store.getLocation().getLongitude());
-        
+
         return NearbySearchRequest.newBuilder()
-            .withKeyword(store.getName().toLowerCase())
-            .withLocation(location)
-            .withRadiusInMeters(DEFAULT_RADIUS)
-            .build();
-        
+                                  .withKeyword(store.getName().toLowerCase())
+                                  .withLocation(location)
+                                  .withRadiusInMeters(DEFAULT_RADIUS)
+                                  .build();
+
     }
-    
+
     private void checkStore(Store store)
     {
         checkThat(store)
-            .is(notNull())
-            .is(validStore());
+                .is(notNull())
+                .is(validStore());
     }
-    
+
     private void makeNoteThatGoogleSearchFailed(GooglePlacesException ex, NearbySearchRequest request, Store store)
     {
         LOG.error("Failed to execute Google Place Search for store: [{}]", store, ex);
-        
+
         aroma.begin().titled("Google API Failed")
-            .withBody("Failed to execute Google Place Search. \n\nStore:\n{}\n\nRequest:\n{}\n\n{}", store, request, ex)
-            .withPriority(Priority.HIGH)
-            .send();
+             .withBody("Failed to execute Google Place Search. \n\nStore:\n{}\n\nRequest:\n{}\n\n{}", store, request, ex)
+             .withPriority(Priority.HIGH)
+             .send();
     }
-    
+
     private void makeNoteThatNoPlacesFoundFor(Store store, NearbySearchRequest request)
     {
         LOG.warn("No matches found for store: [{}]", store);
-        
+
         aroma.begin().titled("No Places Found")
-            .withBody("No Google Places found for Store:\n\n{}\n\nFor Request:\n{}", store, request)
-            .withPriority(Priority.MEDIUM)
-            .send();
+             .withBody("No Google Places found for Store:\n\n{}\n\nFor Request:\n{}", store, request)
+             .withPriority(Priority.MEDIUM)
+             .send();
     }
-    
+
     private void makeNoteThatNoMatchesFoundFor(Store store, List<Place> places)
     {
         LOG.warn("No matches found for store: [{}]", store);
-        
+
         aroma.begin().titled("No Store Matches")
-            .withBody("No Google Places found matching store:\n\n{}\n\nAmong results:\n{}", store, places)
-            .withPriority(Priority.MEDIUM)
-            .send();
+             .withBody("No Google Places found matching store:\n\n{}\n\nAmong results:\n{}", store, places)
+             .withPriority(Priority.MEDIUM)
+             .send();
     }
-    
+
     private void makeNoteThatPlaceMatched(Store store, Place place)
     {
         LOG.debug("Store {} matched with Place | {}", store, place);
-        
+
         aroma.begin().titled("Google Places Match Found")
-            .withBody("Store \n\n{}\n\nMatched with:\n\n{}", store, place)
-            .withPriority(Priority.LOW)
-            .send();
+             .withBody("Store \n\n{}\n\nMatched with:\n\n{}", store, place)
+             .withPriority(Priority.LOW)
+             .send();
     }
 }

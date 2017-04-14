c30228ce759e87a73300f78f6c60e675e9260a3f
Wellington Moreno <jwellington.moreno@gmail.com>
2017-04-14 16:16:23 -0700

    Adds license information

--- src/main/java/tech/blacksource/blacknectar/service/operations/ebt/GetStateInfoOperation.java
+++ src/main/java/tech/blacksource/blacknectar/service/operations/ebt/GetStateInfoOperation.java
@@ -1,20 +1,34 @@
+/*
+ * Copyright 2017 BlackSource, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package tech.blacksource.blacknectar.service.operations.ebt;
 
-import java.util.Set;
-
-import javax.inject.Inject;
-
 import com.google.gson.*;
+import java.util.Set;
+import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sir.wellington.alchemy.collections.sets.Sets;
 import spark.*;
 import tech.aroma.client.Aroma;
 import tech.blacksource.blacknectar.ebt.balance.*;
-import tech.blacksource.blacknectar.service.json.JSON;
 import tech.blacksource.blacknectar.service.data.MediaTypes;
 import tech.blacksource.blacknectar.service.exceptions.BadArgumentException;
 import tech.blacksource.blacknectar.service.exceptions.UnsupportedStateException;
+import tech.blacksource.blacknectar.service.json.JSON;
 import tech.blacksource.blacknectar.service.operations.Parameters;
 import tech.sirwellington.alchemy.arguments.AlchemyAssertion;
 
--- src/test/java/tech/blacksource/blacknectar/service/operations/ebt/GetStateInfoOperationTest.java
+++ src/test/java/tech/blacksource/blacknectar/service/operations/ebt/GetStateInfoOperationTest.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2017 BlackSource, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package tech.blacksource.blacknectar.service.operations.ebt;
 
+import com.google.gson.*;
 import java.util.List;
 import java.util.Set;
-
-import com.google.gson.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,9 +28,9 @@ import spark.Request;
 import spark.Response;
 import tech.aroma.client.Aroma;
 import tech.blacksource.blacknectar.ebt.balance.*;
-import tech.blacksource.blacknectar.service.json.JSON;
 import tech.blacksource.blacknectar.service.data.MediaTypes;
 import tech.blacksource.blacknectar.service.exceptions.*;
+import tech.blacksource.blacknectar.service.json.JSON;
 import tech.blacksource.blacknectar.service.operations.Parameters;
 import tech.sirwellington.alchemy.generator.*;
 import tech.sirwellington.alchemy.test.junit.runners.*;

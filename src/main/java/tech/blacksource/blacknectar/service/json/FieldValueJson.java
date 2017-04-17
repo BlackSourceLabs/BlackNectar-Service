237ec0ddf857e0d670d7d2080a2aa59048a1820c
Wellington Moreno <jwellington.moreno@gmail.com>
2017-04-16 20:50:57 -0700

    Expands on FieldValueJson type

--- src/main/java/tech/blacksource/blacknectar/service/json/FieldValueJson.java
+++ src/main/java/tech/blacksource/blacknectar/service/json/FieldValueJson.java
@@ -21,8 +21,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.blacksource.blacknectar.ebt.balance.Field;
 import tech.blacksource.blacknectar.ebt.balance.FieldValue;
-import tech.sirwellington.alchemy.annotations.arguments.NonEmpty;
-import tech.sirwellington.alchemy.annotations.arguments.Required;
+import tech.sirwellington.alchemy.annotations.access.Internal;
+import tech.sirwellington.alchemy.annotations.arguments.*;
+import tech.sirwellington.alchemy.arguments.Checks;
 
 import static tech.blacksource.blacknectar.service.BlackNectarAssertions.hasField;
 import static tech.sirwellington.alchemy.arguments.Arguments.*;
@@ -30,14 +31,20 @@ import static tech.sirwellington.alchemy.arguments.assertions.Assertions.notNull
 import static tech.sirwellington.alchemy.arguments.assertions.StringAssertions.nonEmptyString;
 
 /**
+ * This is an internal representation of {@link FieldValue}.
+ * This shortened version which is received from clients contains just the bare minimum
+ * information required.
+ *
  * @author SirWellington
  */
+@Internal
 final class FieldValueJson implements AsJson
 {
     private final static Logger LOG = LoggerFactory.getLogger(FieldValueJson.class);
 
     private final String name;
     private final String value;
+    private final Field.FieldType fieldType;
     private final JsonObject json;
 
     /**
@@ -51,20 +58,27 @@ final class FieldValueJson implements AsJson
         checkThat(json)
                 .is(notNull())
                 .is(hasField(Keys.NAME))
-                .is(hasField(Keys.VALUE));
+                .is(hasField(Keys.VALUE))
+                .is(hasField(Keys.TYPE));
 
         String name = json.get(Keys.NAME).getAsString();
         String value = json.get(Keys.VALUE).getAsString();
+        String fieldTypeString = json.get(Keys.TYPE).getAsString();
 
-        return new FieldValueJson(name, value);
+        Field.FieldType fieldType = extractFieldTypeFrom(fieldTypeString);
+
+        return new FieldValueJson(name, value, fieldType);
     }
 
-    FieldValueJson(@NonEmpty String name, @NonEmpty String value)
+    FieldValueJson(@NonEmpty String name, @NonEmpty String value, @Required Field.FieldType fieldType)
     {
         checkThat(name, value).are(nonEmptyString());
+        checkThat(fieldType).is(notNull());
 
         this.name = name;
         this.value = value;
+        this.fieldType = fieldType;
+
         this.json = toJson();
     }
 
@@ -73,13 +87,14 @@ final class FieldValueJson implements AsJson
         JsonObject json = new JsonObject();
         json.addProperty(Keys.NAME, name);
         json.addProperty(Keys.VALUE, value);
+        json.addProperty(Keys.TYPE, fieldType.toString());
 
         return json;
     }
 
     FieldValue toNative()
     {
-        Field field = new Field(name, Field.FieldType.OTHER);
+        Field field = new Field(name, fieldType);
 
         return new FieldValue(field, value);
     }
@@ -90,9 +105,29 @@ final class FieldValueJson implements AsJson
         return json;
     }
 
+
+    private static Field.FieldType extractFieldTypeFrom(String fieldTypeString)
+    {
+        if (Checks.isNullOrEmpty(fieldTypeString))
+        {
+            return Field.FieldType.OTHER;
+        }
+
+        try
+        {
+            return Field.FieldType.valueOf(fieldTypeString);
+        }
+        catch (RuntimeException ex)
+        {
+            return Field.FieldType.OTHER;
+        }
+    }
+
+
     static class Keys
     {
         static final String NAME = "name";
         static final String VALUE = "value";
+        static final String TYPE = "type";
     }
 }
--- src/test/java/tech/blacksource/blacknectar/service/json/FieldValueJsonTest.java
+++ src/test/java/tech/blacksource/blacknectar/service/json/FieldValueJsonTest.java
@@ -1,4 +1,4 @@
-package tech.blacksource.blacknectar.service.json;/*
+/*
  * 
  * Copyright 2017 BlackSource, LLC.
  *
@@ -14,13 +14,18 @@ package tech.blacksource.blacknectar.service.json;/*
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package tech.blacksource.blacknectar.service.json;
 
 import com.google.gson.JsonObject;
 import org.hamcrest.Matchers;
 import org.junit.*;
 import org.junit.runner.RunWith;
+import tech.blacksource.blacknectar.ebt.balance.Field;
+import tech.blacksource.blacknectar.ebt.balance.FieldValue;
 import tech.sirwellington.alchemy.test.junit.runners.*;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static tech.sirwellington.alchemy.test.junit.ThrowableAssertion.*;
 
 /**
@@ -36,6 +41,9 @@ public class FieldValueJsonTest
     @GenerateString
     private String value;
 
+    @GenerateEnum
+    private Field.FieldType fieldType;
+
     private JsonObject expected;
 
     private FieldValueJson instance;
@@ -45,23 +53,37 @@ public class FieldValueJsonTest
     public void setUp() throws Exception
     {
         setupData();
-        instance = new FieldValueJson(name, value);
+        instance = new FieldValueJson(name, value, fieldType);
     }
 
     @DontRepeat
     @Test
     public void testConstructorWithBadArgs() throws Exception
     {
-        assertThrows(() -> new FieldValueJson("", value));
-        assertThrows(() -> new FieldValueJson(name, ""));
+        assertThrows(() -> new FieldValueJson(null, value, fieldType));
+        assertThrows(() -> new FieldValueJson(name, null, fieldType));
+        assertThrows(() -> new FieldValueJson(name, value, null));
+
+        assertThrows(() -> new FieldValueJson("", value, fieldType));
+        assertThrows(() -> new FieldValueJson(name, "", fieldType));
     }
 
     @Test
     public void asJson() throws Exception
     {
         JsonObject result = instance.asJson();
-        Assert.assertThat(result, Matchers.notNullValue());
-        Assert.assertThat(result, Matchers.is(expected));
+        assertThat(result, Matchers.notNullValue());
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testAsNativeType() throws Exception
+    {
+        FieldValue result = instance.toNative();
+
+        assertThat(result.getValue(), is(value));
+        assertThat(result.getField().getName(), is(name));
+        assertThat(result.getField().getType(), is(fieldType));
     }
 
     private void setupData()
@@ -69,6 +91,7 @@ public class FieldValueJsonTest
         expected = new JsonObject();
         expected.addProperty(FieldValueJson.Keys.NAME, name);
         expected.addProperty(FieldValueJson.Keys.VALUE, value);
+        expected.addProperty(FieldValueJson.Keys.TYPE, fieldType.toString());
     }
 
     private void setupMocks()
